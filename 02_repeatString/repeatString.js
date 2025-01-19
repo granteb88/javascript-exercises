7dc67b38efe68425ec62b7e1f4cafedf01105a23
granteb88 <96847692+granteb88@users.noreply.github.com>
2025-01-19 18:39:18 +0100

    Complete 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
 
-};
+const repeatString = function(a, b) {
+    if (b < 0) return "ERROR";
+    text = "";
+    for (let i = 0; i < b; i++) {
+        text += a;
+        };
+        return text;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
